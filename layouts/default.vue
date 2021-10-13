43c85f6ddafb4ff9dac8fd9fb538e9af099e073a
RewBugLag <rewrew2000@gmail.com>
2021-10-13 17:47:50 +0700

    [Fixed] styling disappearance when reload other than index.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,13 @@
   </div>
 </template>
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Prompt');
 body{
-   font-family: 'Prompt', sans-serif;
+  font-family: 'Prompt', sans-serif;
+  background: url(https://img.freepik.com/free-photo/vaccine-vial-with-needle-syringe-background_53876-98427.jpg?size=626&ext=jpg) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -13,14 +13,4 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Prompt');
-
-body {
-  font-family: 'Prompt', sans-serif;
-  background: url(https://img.freepik.com/free-photo/vaccine-vial-with-needle-syringe-background_53876-98427.jpg?size=626&ext=jpg) no-repeat center center fixed;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-}
 </style>
