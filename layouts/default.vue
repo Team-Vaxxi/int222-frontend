f4b034f03a0c6e08205e387364bed3cc88ae966e
pooripro <steampro35200@gmail.com>
2021-10-23 22:23:04 +0700

    [ADDED] add authen to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <default-navbar></default-navbar>
-    <Nuxt /> 
+    <authentication></authentication>
   </div>
 </template>
 <script>
 import DefaultNavbar from '~/components/DefaultNavbar.vue'
+import authentication from '~/pages/authen/authentication.vue'
 export default {
+  components: { authentication },
   component: { DefaultNavbar}
 }
 </script>
