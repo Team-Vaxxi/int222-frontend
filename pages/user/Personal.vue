847add4f218498bcfa96d714293a428ece58d00c
RewBugLag <rewrew2000@gmail.com>
2021-11-04 23:15:32 +0700

    [Changed] static data to dynamic data

--- pages/user/Personal.vue
+++ pages/user/Personal.vue
@@ -1,20 +1,10 @@
 <template>
   <div class="w-full">
-    <div class="
-          grid 
-          md:grid-cols-5
-          md:justify-items-center
-          md:mb-3
-          w-full">
-
+    <div class="grid md:grid-cols-5 md:justify-items-center md:mb-3 w-full">
       <!-- insert value each input by jwttoken user -->
-      <bg-card class="
-      md:col-start-2 
-      md:col-end-5 
-      md:mt-5 p-2 
-      md:p-5
-      md:w-10/12
-      ">
+      <bg-card
+        class="md:col-start-2 md:col-end-5 md:mt-5 p-2 md:p-5 md:w-10/12"
+      >
         <div class="w-full flex justify-center">
           <form class="w-full">
             <h1
@@ -57,9 +47,11 @@
                   px-4
                   mb-3
                   leading-tight
-                  focus:outline-none focus:bg-white focus:border-gray-500
+                  focus:outline-none
+                  focus:bg-white
+                  focus:border-gray-500
                 "
-                value="1100401044766"
+                :value="user.idCard"
                 type="text"
                 readonly
               />
@@ -92,9 +84,11 @@
                   py-3
                   px-4
                   leading-tight
-                  focus:outline-none focus:bg-white focus:border-gray-500
+                  focus:outline-none
+                  focus:bg-white
+                  focus:border-gray-500
                 "
-                value="Puwa"
+                :value="user.name"
                 type="text"
                 readonly
               />
@@ -126,38 +120,38 @@
                   py-3
                   px-4
                   leading-tight
-                  focus:outline-none focus:bg-white focus:border-gray-500
+                  focus:outline-none
+                  focus:bg-white
+                  focus:border-gray-500
                 "
-                value="Termnuphan"
+                :value="user.surname"
                 type="text"
                 readonly
               />
             </div>
             <!-- gender -->
             <label class="block text-gray-700 font-bold mb-2 mt-3">เพศ</label>
-            <div class="flex">
-              <div class="flex items-center mb-2 mr-4">
-                <input
-                  id="male"
-                  type="radio"
-                  name="male"
-                  checked="checked"
-                  class="h-4 w-4 text-gray-700 px-3 py-3 border rounded mr-2"
-                  onclick="return false"
-                />
-                <label class="text-gray-700">ชาย</label>
-              </div>
-              <div class="flex items-center mb-3">
-                <input
-                  id="male"
-                  type="radio"
-                  name="male"
-                  class="h-4 w-4 text-gray-700 px-3 py-3 border rounded mr-2"
-                  onclick="return false"
-                />
-                <label class="text-gray-700">หญิง</label>
-              </div>
-            </div>
+            <input
+              class="
+                appearance-none
+                block
+                w-full
+                bg-gray-200
+                text-gray-700
+                border border-gray-200
+                rounded
+                py-3
+                px-4
+                mb-3
+                leading-tight
+                focus:outline-none
+                focus:bg-white
+                focus:border-gray-500
+              "
+              type="text"
+              :value="user.gender"
+              readonly
+            />
             <!-- address -->
             <div class="flex flex-wrap -mx-3 mt-3">
               <div class="w-full px-3">
@@ -187,9 +181,11 @@
                     py-3
                     px-4
                     leading-tight
-                    focus:outline-none focus:bg-white focus:border-gray-500
+                    focus:outline-none
+                    focus:bg-white
+                    focus:border-gray-500
                   "
-                  value="xxxxxxxxxxxxxxxxxxxx"
+                  :value="user.address"
                   readonly
                 />
               </div>
@@ -212,9 +208,11 @@
                     py-3
                     px-4
                     leading-tight
-                    focus:outline-none focus:bg-white focus:border-gray-500
+                    focus:outline-none
+                    focus:bg-white
+                    focus:border-gray-500
                   "
-                  value="01-01-01"
+                  :value="user.dob"
                   readonly
                 />
               </div>
@@ -247,15 +245,17 @@
                     px-4
                     mb-3
                     leading-tight
-                    focus:outline-none focus:bg-white focus:border-gray-500
+                    focus:outline-none
+                    focus:bg-white
+                    focus:border-gray-500
                   "
                   type="text"
-                  value="098263123"
+                  :value="user.tel"
                   readonly
                 />
               </div>
             </div>
-            <!-- สถานการจองวัคซีน -->
+            <!-- สถานะการจองวัคซีน -->
             <!-- v-if -->
             <div class="flex flex-wrap -mx-3 mt-3">
               <div class="w-full px-3">
@@ -271,82 +271,158 @@
                 >
                   สถานะการจองวัคซีน
                 </label>
-                <p class="text-red-500">คุณยังไม่ได้จองวัคซีน</p>
+                <p v-if="!user.vaccine" class="text-red-500">
+                  คุณยังไม่ได้จองวัคซีน
+                </p>
+                <p v-else class="text-green-500">คุณจองวัคซีนเรียบร้อยแล้ว</p>
+              </div>
+            </div>
+            <!-- วัคซีนที่จอง -->
+            <div v-if="user.vaccine" class="flex flex-wrap -mx-3 mt-3">
+              <div class="w-full px-3">
+                <label
+                  class="
+                    block
+                    uppercase
+                    tracking-wide
+                    text-gray-700
+                    font-bold
+                    mb-2
+                  "
+                >
+                  วัคซีนที่จอง
+                </label>
+                <input
+                  class="
+                    appearance-none
+                    block
+                    w-full
+                    bg-gray-200
+                    text-gray-700
+                    border border-gray-200
+                    rounded
+                    py-3
+                    px-4
+                    mb-3
+                    leading-tight
+                    focus:outline-none
+                    focus:bg-white
+                    focus:border-gray-500
+                  "
+                  type="text"
+                  :value="user.vaccine"
+                  readonly
+                />
+              </div>
+            </div>
+            <!-- สถานที่ฉีดวัคซีน -->
+            <div v-if="user.location" class="flex flex-wrap -mx-3 mt-3">
+              <div class="w-full px-3">
+                <label
+                  class="
+                    block
+                    uppercase
+                    tracking-wide
+                    text-gray-700
+                    font-bold
+                    mb-2
+                  "
+                >
+                  สถานที่ฉีดวัคซีน
+                </label>
+                <input
+                  class="
+                    appearance-none
+                    block
+                    w-full
+                    bg-gray-200
+                    text-gray-700
+                    border border-gray-200
+                    rounded
+                    py-3
+                    px-4
+                    mb-3
+                    leading-tight
+                    focus:outline-none
+                    focus:bg-white
+                    focus:border-gray-500
+                  "
+                  type="text"
+                  :value="user.location"
+                  readonly
+                />
               </div>
             </div>
             <div class="mt-10">
-            <div>
-              <Nuxt-link to="/user/home">
-              <button
-                type="submit"
-                class="
-                  group
-                  relative
-                  w-full
-                  flex
-                  justify-center
-                  py-2
-                  px-4
-                  border border-transparent
-                  text-lg
-                  font-medium
-                  rounded-md
-                  text-white
-                  bg-indigo-600
-                  hover:bg-indigo-700
-                  focus:outline-none
-                  focus:ring-2
-                  focus:ring-offset-2
-                  focus:ring-indigo-500
-                "
-              >
-                <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-                </span>
-                กลับเข้าสู่หน้าเลือกวัคซีน
-              </button>
-              </Nuxt-link>
-            </div>
-            <div class="mt-2" @click="logout">
-              <Nuxt-link to="/">
-              <button
-                  type="button"
-                  class="
-                    group
-                    relative
-                    w-full
-                    flex
-                    justify-center
-                    py-2
-                    px-4
-                    border border-transparent
-                    text-l
-                    font-medium
-                    rounded-md
-                    text-indigo-600
-                    bg-white
-                    border-indigo-700
-                    hover:bg-indigo-700
-                    hover:text-white
-                    focus:outline-none
-                    focus:ring-2
-                    focus:ring-offset-2
-                    focus:ring-indigo-500
-                    mt-2
-                  "
-                >
-                  <span
-                    class="absolute left-0 inset-y-0 flex items-center pl-3"
+              <div>
+                <Nuxt-link to="/user/home">
+                  <button
+                    type="submit"
+                    class="
+                      group
+                      relative
+                      w-full
+                      flex
+                      justify-center
+                      py-2
+                      px-4
+                      border border-transparent
+                      text-lg
+                      font-medium
+                      rounded-md
+                      text-white
+                      bg-indigo-600
+                      hover:bg-indigo-700
+                      focus:outline-none
+                      focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+                    "
                   >
-                  </span>
-                  ออกจากระบบ
-                </button>
+                    <span
+                      class="absolute left-0 inset-y-0 flex items-center pl-3"
+                    >
+                    </span>
+                    กลับเข้าสู่หน้าเลือกวัคซีน
+                  </button>
                 </Nuxt-link>
+              </div>
+              <div class="mt-2" @click="logout">
+                <Nuxt-link to="/">
+                  <button
+                    type="button"
+                    class="
+                      group
+                      relative
+                      w-full
+                      flex
+                      justify-center
+                      py-2
+                      px-4
+                      border border-transparent
+                      text-l
+                      font-medium
+                      rounded-md
+                      text-indigo-600
+                      bg-white
+                      border-indigo-700
+                      hover:bg-indigo-700
+                      hover:text-white
+                      focus:outline-none
+                      focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+                      mt-2
+                    "
+                  >
+                    <span
+                      class="absolute left-0 inset-y-0 flex items-center pl-3"
+                    >
+                    </span>
+                    ออกจากระบบ
+                  </button>
+                </Nuxt-link>
+              </div>
             </div>
-          </div>
           </form>
         </div>
       </bg-card>
-
     </div>
   </div>
 </template>
@@ -356,12 +432,17 @@ import BgCard from '~/components/BgCard.vue'
 export default {
   components: { BgCard },
   layout: 'user',
+  data() {
+    return {
+      user: this.$auth.user,
+    }
+  },
   methods: {
     async logout() {
-      await this.$auth.logout();
-      this.$router.replace('/authen/authentication');
-    }
-  }
+      await this.$auth.logout()
+      this.$router.replace('/authen/authentication')
+    },
+  },
 }
 </script>
 
